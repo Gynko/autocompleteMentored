b72f7eb98dab6898bc27df62d54df6099721c88b
Yoann G <yoann.3d@gmail.com>
2022-04-15 08:15:48 +0200

    fixed removing results when input search becomes empty

--- src/components/autocomplete.jsx
+++ src/components/autocomplete.jsx
@@ -1,6 +1,7 @@
 import "./autocomplete.styles.css";
 import { useEffect, useState } from "react";
 import { countries } from "../data/countryList.js";
+import { render } from "@testing-library/react";
 
 function Autocomplete() {
   const [typed, setTyped] = useState("");
@@ -11,19 +12,31 @@ function Autocomplete() {
   }
 
   useEffect(() => {
-    console.log("typed", typed);
     if (typed !== "") search(typed);
+    else if (typed === "") setResults([]);
   }, [typed]);
 
   // If first input is G, get all elements of array starting with G
   // step2: first 2 letters
   function search(input) {
     var regexLiteral = new RegExp(`^${input.toLowerCase()}`);
-    var results = countries.filter((element) =>
+    var matches = countries.filter((element) =>
       regexLiteral.test(element.toLowerCase())
     );
-    setResults(results);
-    console.log("results", input, results);
+    setResults(matches);
+    if (input !== "") renderResults();
+  }
+
+  function renderResults() {
+    if (results.length > 0)
+      return (
+        <ul className="autocomplete-list">
+          {results.length > 0
+            ? results.map((entries, index) => <li key={index}>{entries}</li>)
+            : null}
+        </ul>
+      );
+    else return null;
   }
 
   return (
@@ -35,13 +48,7 @@ function Autocomplete() {
           placeholder="Country"
           onChange={onEnteringInput}
         />
-        <div className="autocomplete-items">
-          <ul className="autocomplete-list">
-            {results.length > 0
-              ? results.map((entries, index) => <li key={index}>{entries}</li>)
-              : null}
-          </ul>
-        </div>
+        <div className="autocomplete-items">{renderResults()}</div>
       </div>
       <input type="submit" />
     </form>
